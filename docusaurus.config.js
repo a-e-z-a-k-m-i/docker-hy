6b6944a2cfa1c3e98fdf2b9839b9c3b8f4d04117
a-e-z-a-k-m-i <qwnrzxbstcpue@duck.com>
2024-05-22 01:01:34 +0000

    update app

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'DevOps with Docker',
-  tagline: 'Containers for Beginners',
+  tagline: 'Containers for Experts',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
